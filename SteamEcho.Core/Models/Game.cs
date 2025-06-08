da79ffca8edfc24e3b3a42ddab2064e821ae2edc
Timothée Massa <timothee.massa.perso@gmail.com>
2025-06-08 21:57:22 +0200

    feat: create Game class

---
+++ SteamEcho.Core/Models/Game.cs
@@ -0,0 +1,13 @@
+namespace SteamEcho.Core.Models;
+
+public class Game(string name, string executablePath)
+{
+    public string Name { get; set; } = name;
+    public string ExecutablePath { get; set; } = executablePath;
+    public List<Achievement> Achievements { get; set; } = [];
+
+    public void AddAchievement(Achievement achievement)
+    {
+        Achievements.Add(achievement);
+    }
+}
