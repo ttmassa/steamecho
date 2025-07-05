80bbad596484be1a7ae483d3f2f7c01659378661
Timothée Massa <timothee.massa.perso@gmail.com>
2025-07-05 11:27:43 +0200

    refactor: make some refactors

--- SteamEcho.App/Services/NotificationService.cs
+++ SteamEcho.App/Services/NotificationService.cs
@@ -1,25 +1,21 @@
 using SteamEcho.App.Views;
 using SteamEcho.Core.Models;
 using SteamEcho.Core.Services;
-using System;
-using System.Threading.Tasks;
-using System.Windows;
 
-namespace SteamEcho.App.Services
+namespace SteamEcho.App.Services;
+
+public class NotificationService : INotificationService
 {
-    public class NotificationService : INotificationService
+    public async void ShowNotification(Achievement achievement)
     {
-        public async void ShowNotification(Achievement achievement)
+        var notificationWindow = new AchievementNotificationWindow
         {
-            var notificationWindow = new AchievementNotificationWindow
-            {
-                DataContext = achievement
-            };
-            notificationWindow.Show();
+            DataContext = achievement
+        };
+        notificationWindow.Show();
 
-            // Automatically close the notification after 7 seconds
-            await Task.Delay(TimeSpan.FromSeconds(7));
-            notificationWindow.Close();
-        }
+        // Automatically close the notification after 7 seconds
+        await Task.Delay(TimeSpan.FromSeconds(7));
+        notificationWindow.Close();
     }
 }
